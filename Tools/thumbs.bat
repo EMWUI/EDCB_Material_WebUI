516b5ec4e8ebbeed2c71d7a98fa081b4f45ebf33
EMWUI <rodochimitherman@outlook.com>
2016-05-12 22:00:24 +0900

    ※ffmpegとreadexのデフォルトをToolsフォルダに変更
    サムネ作成用バッチ例を追加

---
+++ Tools/thumbs.bat
@@ -0,0 +1,25 @@
+﻿@echo off
+
+setlocal
+
+rem サムネフォルダ
+set OUT=%~dp0\..\HttpPublic\thumbs
+
+rem ffmpeg.exe
+set FFM=%~dp0\ffmpeg.exe
+
+if not exist "%OUT%" mkdir "%OUT%"
+
+:loop
+
+if "%~1" == "" goto end
+
+"%FFM%" -i "%~1" -ss 15 -vframes 1 -f image2 -s 480x270 "%OUT%\%~n1.jpg"
+
+shift /1
+
+goto loop
+
+:end
+
+endlocal
