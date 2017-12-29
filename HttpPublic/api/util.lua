a74cf34112a492dd73f13558f1c177bbd3e8daa4
EMWUI <rodochimitherman@outlook.com>
2017-12-30 01:10:15 +0900

    POSTメッセージボディの読み込みと検査を一体化
    https://github.com/xtne6f/EDCB/commit/eef4ce3f214adb1ebb28ef8d48b1fc00145c2f9c

--- HttpPublic/api/util.lua
+++ HttpPublic/api/util.lua
@@ -1,5 +1,5 @@
 ﻿--POSTメッセージボディをすべて読む
-function ReadPost()
+function AssertPost()
   local post, s
   if mg.request_info.request_method=='POST' then
     post=''
@@ -8,8 +8,9 @@ function ReadPost()
       post=post..(s or '')
     until not s
     if #post~=mg.request_info.content_length then
-      post=nil
+      post=''
     end
+    AssertCsrf(post)
   end
   return post
 end
