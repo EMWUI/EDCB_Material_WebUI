fd4625536d2d3242aa6b3976df562541d3d44963
EMWUI <rodochimitherman@outlook.com>
2018-01-18 22:02:03 +0900

    ログを表示するLuaスクリプトのサロゲートペア対策
    https://github.com/xtne6f/EDCB/commit/efa47229cf994f973079114d46cd631f1fd2eb4d

--- HttpPublic/debuglog.lua
+++ HttpPublic/debuglog.lua
@@ -9,11 +9,20 @@ else
     ofs=math.floor(math.max(fsize/2-1-math.min(math.max(c,0),1e7),0))
     f:seek('set',2+ofs*2)
     a=f:read('*a')
-    --utf-16le(without surrogates) to utf-8
+    --utf-16le to utf-8
     a=a:gsub('..',function (x)
-      x=string.byte(x,1)+string.byte(x,2)*256
+      x=x:byte(1)+x:byte(2)*256
+      if 0xd800<=x and x<=0xdbff then
+        hi=x
+        return ''
+      end
+      if 0xdc00<=x and x<=0xdfff and hi then
+        x=0x10000+(hi-0xd800)*0x400+x-0xdc00
+      end
+      hi=nil
       return x<0x80 and string.char(x) or x<0x800 and string.char(0xc0+math.floor(x/64),0x80+x%64)
-        or string.char(0xe0+math.floor(x/4096),0x80+math.floor(x/64)%64,0x80+x%64)
+        or x<0x10000 and string.char(0xe0+math.floor(x/4096),0x80+math.floor(x/64)%64,0x80+x%64)
+        or string.char(0xf0+math.floor(x/0x40000),0x80+math.floor(x/4096)%64,0x80+math.floor(x/64)%64,0x80+x%64)
     end)
     if ofs~=0 then
       a=a:gsub('^[^\n]*\n','')
