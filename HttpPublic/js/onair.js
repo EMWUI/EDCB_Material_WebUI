4a138420730ebb3bed269503201fda88492a795a
EMWUI <rodochimitherman@outlook.com>
2018-01-27 17:22:37 +0900

    Revert "放送中一覧で予約を反映させてなかったのを修正"
    This reverts commit 77e4564540aa64a6f2ae26ad5436174c2d585a4b.

--- HttpPublic/js/onair.js
+++ HttpPublic/js/onair.js
@@ -3,6 +3,11 @@
 	return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
 }
 
+function updateEPG(obj){
+	var time = obj.data('end') - new Date().getTime();
+	setTimeout(function(){getEPG(obj);}, time);
+}
+
 function getEvent(event){
 	var title = event.children('event_name').text().replace(/　/g,' ').replace(/\[(新|終|再|交|映|手|声|多|字|二|Ｓ|Ｂ|SS|無|Ｃ|S1|S2|S3|MV|双|デ|Ｄ|Ｎ|Ｗ|Ｐ|HV|SD|天|解|料|前|後|初|生|販|吹|PPV|演|移|他|収)\]/g,'<span class="mark mdl-color--accent mdl-color-text--accent-contrast">$1</span>');
 	var textTime = event.children('startTime').text();
@@ -21,7 +26,7 @@ function getEvent(event){
 function getEPG(obj){
 	var data = obj.data();
 	$.get(root + 'api/EnumEventInfo?onair=&onid=' + data.onid + '&tsid=' + data.tsid + '&sid=' + data.sid).done(function(xhr){
-		var event, next, data;
+		var event,  next, data;
 		if ($(xhr).find('eventinfo').length > 0){
 			event=getEvent( $($(xhr).find('eventinfo')[0]) );
 			next=getEvent( $($(xhr).find('eventinfo')[1]) );
@@ -36,6 +41,7 @@ function getEPG(obj){
 			obj.find('.title').html(event.title);
 			obj.find('.nextstartTime').text(next.textTime).next('.nextendTime').text('～' + getTime(next.endTime));
 			obj.find('.nexttitle').html(next.title);
+			updateEPG(obj);
 		}
 	});
 }
@@ -44,7 +50,7 @@ setInterval(function(){
 	$('.mdl-layout__tab-panel.is-active>.mdl-list__item').each(function(){
 		var data = $(this).data();
 		if (data.end < Date.now()){
-			getEPG($(this));
+			updateEPG($(this));
 		}else{
 			var progress = (Date.now()/1000 - data.start) / data.duration  * 100;
 			$(this).children('.mdl-progress').get(0).MaterialProgress.setProgress(progress);
@@ -53,13 +59,13 @@ setInterval(function(){
 },1000);
 
 $(function(){
+	$('.mdl-layout__tab-panel.is-active>.mdl-list__item').each(function(){
+		updateEPG($(this));
+	});
+
 	$('span.epginfo').click(function(){
 		var data = $(this).parents('li').clone(true).data();
-		if ($(this).hasClass('next')){
-			data.next = true;
-			data.id = data.nextid;
-			data.eid = data.nexteid;
-		}
+		if ($(this).hasClass('next')) data.eid = data.nexteid;
 
 		if (data.eid!=0){
 			if ($(this).hasClass('panel')){
